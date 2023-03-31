55e04ac622594a44c56d572d0201178b66a8c4e1
FrancoCornejoAndreani <fcornejo@andreani.com>
2023-03-31 17:27:57 -0300

    funcion para recuperar token x id

--- callToContract.js
+++ callToContract.js
@@ -1,18 +1,26 @@
 require('dotenv').config();
+const Web3 = require('web3');
+const contractAbi = require('./wooyABI.json');
+const web3 = new Web3('https://rpc-mumbai.maticvigil.com');
 
 const ownerPrivateKey = process.env.OWNER_PRIVATE_KEY;
 const ownerAccount = process.env.OWNER_ACCOUNT;
 const contractAddress = process.env.CONTRACT_ADDRESS;
-const tokenURI = ""; //falta el ips
+const tokenURI = process.env.TOKEN_URI; //falta el ips
+
+const contractInstance = new web3.eth.Contract(contractAbi, contractAddress);
 
 //----LECTURA DE LA BASE DE DATOS
 //----CONST CON ARRAY Y LA CANTIDAD DE EMAILS DE LA BASE DE DATOS
 //const arrayEmailBd = ["32w2eqwe", "dsa3qwew", "ewe2e2ew232"]
+const email = "cornejo.francodavid@gmail.com";
+const data = web3.utils.utf8ToHex(email);
+//console.log(data)
 
-
-export function callSafeMint(tokenUser){
-    tokenUser.forEach(userTokenValue => {
-        contractInstance.methods.safeMint(ownerAccount, tokenURI, userTokenValue).send({from: ownerAccount, gas: 300000})
+ function callSafeMint(tokenUser){
+    tokenUser.forEach(userTokenValue => { 
+    web3.eth.accounts.wallet.add(ownerPrivateKey);
+        contractInstance.methods.safeMint(ownerAccount, tokenURI, data).send({from: ownerAccount, gas: 300000})
         .on('transactionHash', function(hash){
             console.log('Transacción enviada: ' + hash);
         })
@@ -22,14 +30,16 @@ export function callSafeMint(tokenUser){
         .on('error', function(error){
             console.error(error);
         });
-    })
+     })
+} 
+
+async function recoveryToken(){
+    const recoveryTokenIdByHash = await contractInstance.methods.getTokenIdByUserToken(data).call()
+    console.log(recoveryTokenIdByHash)
 }
 
-/*Aca tengo que recuperar el tokenID del NFT para pasarlo como parametro
-    a la funcion safeTransferFrom para identificar el NFT
- */
-export function callSafeTransferFrom(accountUser){
-    contractInstance.methods.safeTransferFrom(ownerAccount, accountUser, 1).send({ from: ownerAccount, gas: 300000})
+function callSafeTransferFrom(accountUser, tokenID){
+    contractInstance.methods.safeTransferFrom(ownerAccount, accountUser, tokenID).send({ from: ownerAccount, gas: 300000})
     .then((receipt) => {
       console.log('La transferencia se realizó con éxito');
     })
