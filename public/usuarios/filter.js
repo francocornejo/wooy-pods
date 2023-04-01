52c8a4d6e5416beac7b233bfd506728bdc8f2976
Santiago Nielsen <santiagodn3@gmail.com>
2023-04-01 04:30:42 -0300

    ajustes form

--- public/usuarios/filter.js
+++ public/usuarios/filter.js
@@ -3,7 +3,6 @@ import { UsersRegister } from "../../models/usuarioSchema.js"
 export const UsuarioRender = async (req, res) => {
     try {
         const arrayUsers = await UsersRegister.find().lean()
-        console.log(typeof arrayUsers)
         res.json(arrayUsers)
 
     } catch (error) {
