3b821658b567289e3a07825b2f3af1c453d5de49
FrancoCornejoAndreani <fcornejo@andreani.com>
2023-04-02 15:06:16 -0300

    correccion contrato

--- callToContract.js
+++ callToContract.js
@@ -1,5 +1,6 @@
 import dotenv from 'dotenv';
 import Web3 from 'web3';
+import { ethers } from "ethers";
 import contractAbi from './abi/wooyABI.json' assert { type: "json" };
 import { UsersRegister } from './models/usuarioSchema.js';
 dotenv.config();
@@ -14,28 +15,33 @@ const tokenURI = process.env.TOKEN_URI;
 const contractInstance = new web3.eth.Contract(contractAbi, contractAddress);
 
 //Minteamos PODs por cada usuario registrado en la base de datos
-export async function callSafeMint(req, res){
+
+ export async function callSafeMint(req, res){
     const usersBd = await UsersRegister.find().lean()
+    web3.eth.accounts.wallet.add(ownerPrivateKey);
     if(usersBd == undefined || usersBd == ""){
         console.log("Su base de dato se encuentra vacia!")
     }
     for (const userTokenValue of usersBd) { 
-        const hashEmail = web3.utils.utf8ToHex(userTokenValue.email)
-        console.log("HASHEMAIL: ", hashEmail)
-        web3.eth.accounts.wallet.add(ownerPrivateKey);
-        await contractInstance.methods.safeMint(ownerAccount, tokenURI, hashEmail).send({from: ownerAccount, gas: 300000})
-        .on('transactionHash', function(hash){
-            console.log('Transacción enviada: ' + hash);
-        })
-        .on('receipt', function(receipt){
-            console.log('Transacción confirmada: ' + receipt.transactionHash);
-        })
-        .on('error', function(error){
-            console.error(error);
-        });
+        const hashEmail = ethers.utils.formatBytes32String(userTokenValue.email)
+        console.log("HashEmail: ", hashEmail)
+
+        try{
+            const tx = await contractInstance.methods.safeMint(ownerAccount, tokenURI, hashEmail).
+                send({from: ownerAccount, gas: 300000})
+
+            console.log('Transaccion enviada');
+
+            const receipt = await web3.eth.getTransactionReceipt(tx.transactionHash)
+            console.log('Transaccion confirmada: ' + receipt.transactionHash);
+
+        } catch(error){
+            console.log('Error');
+            console.log(error);
+        }
     }
     res.send("Minteado Completo!")
-} 
+}  
 
 //Recuperamos el tokenID del NFT que esta bindeado con el hash del email 
 export async function recoveryToken(accountUser, emailHash){
